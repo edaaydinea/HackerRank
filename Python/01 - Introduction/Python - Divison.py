ad36739bbb0d153f3df392de6d1bdf76b1af06ce
Eda AYDIN <46783629+edaaydinea@users.noreply.github.com>
2022-01-22 13:25:43 +0300

    Upload Python - 01 - Introduction Solutions

---
+++ Python/01 - Introduction/Python - Divison.py
@@ -0,0 +1,8 @@
+from __future__ import division
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print (a//b)
+    print (a/b)
---
+++ Python/01 - Introduction/Python If-Else.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    if n % 2 == 1 or 6 <= n <= 20:
+        print('Weird')
+    else:
+        print('Not Weird')
