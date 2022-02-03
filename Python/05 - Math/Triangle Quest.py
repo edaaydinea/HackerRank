dfc4fb04e8c8a16ae290118cd1ea9cf140ed09f9
Eda AYDIN <edaaydin.ea@outlook.com>
2022-02-03 17:46:13 +0300

    Add files via upload

--- Python/05 - Math/Triangle Quest.py
+++
@@ -1,7 +0,0 @@
-"""
-Problem:https://www.hackerrank.com/challenges/python-quest-1/problem
-
-Author: Eda AYDIN
-"""
-for i in range(1, int(input())):
-    print((10 ** i // 9) * i)
--- Python/06 - Itertools/Compress the String.py
+++
@@ -1,9 +0,0 @@
-"""
-Problem: https://www.hackerrank.com/challenges/compress-the-string/problem
-
-Author: Eda AYDIN
-"""
-import itertools
-
-for S, k in itertools.groupby(input()):
-    print("(%d, %d)" % (len(list(k)), int(S)), end=' ')
