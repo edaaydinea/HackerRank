a60bb7a65891942992530b1fd04d653ff6381eff
Eda AYDIN <46783629+edaaydinea@users.noreply.github.com>
2022-02-03 15:16:54 +0300

    Add files via upload

---
+++ Algorithms/1 - Warmup/Diagonal Difference.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    primary_diagonal = 0
+    secondary_diagonal = 0
+
+    for i in range(0, len(arr)):
+        primary_diagonal += arr[i][i]
+
+    for j in range(0, len(arr)):
+        secondary_diagonal += arr[j][len(arr) -1 - j]
+
+    return abs(primary_diagonal - secondary_diagonal)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
