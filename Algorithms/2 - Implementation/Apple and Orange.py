c523a7fd9711d3e658885f51853940bf191ac362
Eda AYDIN <edaaydin.ea@outlook.com>
2022-02-04 23:31:08 +0300

    Add Implementation solutions via upload

---
+++ Algorithms/2 - Implementation/Apple and Orange.py
@@ -0,0 +1,59 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countApplesAndOranges' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER s
+#  2. INTEGER t
+#  3. INTEGER a
+#  4. INTEGER b
+#  5. INTEGER_ARRAY apples
+#  6. INTEGER_ARRAY oranges
+#
+
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    # Write your code here
+    apple_count = 0
+    orange_count = 0
+    
+    for apple in apples:
+        if (apple + a >= s and apple + a <= t):
+            apple_count += 1
+    
+    for orange in oranges:
+        if (orange + b <= t and orange + b >= s):
+            orange_count += 1
+    
+    return "\n".join([str(apple_count), str(orange_count)])
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    s = int(first_multiple_input[0])
+
+    t = int(first_multiple_input[1])
+
+    second_multiple_input = input().rstrip().split()
+
+    a = int(second_multiple_input[0])
+
+    b = int(second_multiple_input[1])
+
+    third_multiple_input = input().rstrip().split()
+
+    m = int(third_multiple_input[0])
+
+    n = int(third_multiple_input[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    print(countApplesAndOranges(s, t, a, b, apples, oranges))
---
+++ Algorithms/2 - Implementation/Grading Students.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    for i in range(len(grades)):
+        if grades[i] >= 38:
+            if grades[i] % 5 > 2:
+                while grades[i] % 5 != 0: grades[i] +=1
+    return grades
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
