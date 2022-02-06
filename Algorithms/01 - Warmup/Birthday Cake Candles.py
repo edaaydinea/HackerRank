6fdd642f41b7f4ba24ee649ba97da0be21b98453
Eda AYDIN <edaaydin.ea@outlook.com>
2022-02-06 15:35:55 +0300

    The folder names have name change (1 -> 01) | (2 -> 02).

---
+++ Algorithms/01 - Warmup/Birthday Cake Candles.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    maximum = max(candles)
+    count = 0
+    for i in range(0, len(candles)):
+        if candles[i] == maximum:
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
