86c4d6a961a82904cf6e6f388608f4b90f4b39fd
Eda AYDIN <edaaydin.ea@outlook.com>
2022-02-11 01:19:49 +0300

    Add Funny String.py

---
+++ Algorithms/03 - Strings/Funny String.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'funnyString' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def funnyString(s):
+    # Write your code here
+    r = s[::-1]
+    for i in range(1, len(s)):
+        if abs(ord(s[i]) - ord(s[i - 1])) != abs(ord(r[i]) - ord(r[i - 1])): return "Not Funny"
+    return "Funny"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = funnyString(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
