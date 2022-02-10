702d78b8d7bc63f1632079b9538f213ff9ee7e26
Eda AYDIN <edaaydin.ea@outlook.com>
2022-02-10 19:51:59 +0300

    Add Pangrams.py

---
+++ Algorithms/03 - Strings/Pangrams.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    alpha_set = set(char for char in s.lower() if char.isalpha())
+    return "pangram" if len(alpha_set) == 26 else "not pangram"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
