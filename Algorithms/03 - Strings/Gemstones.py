361a1e13f9cd4971aee85470da6ae8a2f24b26ac
Eda AYDIN <edaaydin.ea@outlook.com>
2022-02-11 01:54:55 +0300

    Add Gemstones.py

---
+++ Algorithms/03 - Strings/Gemstones.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gemstones' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING_ARRAY arr as parameter.
+#
+
+
+def gemstones(arr):
+    # Write your code here
+    gems = set(arr[0])
+
+    for array in arr[1:]:
+        gems &= set(array)
+
+    return len(gems)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = input()
+        arr.append(arr_item)
+
+    result = gemstones(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
