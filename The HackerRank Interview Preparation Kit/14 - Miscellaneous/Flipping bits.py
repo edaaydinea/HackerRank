a8af0792d5f22453cec97e94a9740259dd04207c
Eda AYDIN <edaaydin.ea@outlook.com>
2022-02-11 13:47:10 +0300

    Add Flipping bits.py

---
+++ The HackerRank Interview Preparation Kit/14 - Miscellaneous/Flipping bits.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'flippingBits' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def flippingBits(n):
+    # Write your code here
+    result = 0
+    k = 1
+
+    for _ in range(32):
+        if n & 1 == 0:
+            result += k
+
+        k <<= 1
+        n >>= 1
+
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        result = flippingBits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
