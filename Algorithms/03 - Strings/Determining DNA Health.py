e811c97e8407e707607bb03f9b0d53e29e428061
Eda AYDIN <edaaydin.ea@outlook.com>
2022-02-11 23:37:53 +0300

    Add Determining DNA Health.py

---
+++ Algorithms/03 - Strings/Determining DNA Health.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+from math import inf
+from bisect import bisect_left as bLeft, bisect_right as bRight
+from collections import defaultdict
+
+
+def getHealth(sequence, first, last, largest):
+    h, len_sequence = 0, len(sequence)
+    for f in range(len_sequence):
+        for j in range(1, largest + 1):
+            if f + j > len_sequence:
+                break
+            sub = sequence[f:f + j]
+            if sub not in subs:
+                break
+            if sub not in gMap:
+                continue
+            ids, hs = gMap[sub]
+            h += hs[bRight(ids, last)] - hs[bLeft(ids, first)]
+    return h
+
+
+howGenes = int(input())
+genes = input().rstrip().split()
+healths = list(map(int, input().rstrip().split()))
+howStrands = int(input())
+gMap = defaultdict(lambda: [[], [0]])
+subs = set()
+for id, gene in enumerate(genes):
+    gMap[gene][0].append(id)
+    for j in range(1, min(len(gene), 500) + 1):
+        subs.add(gene[:j])
+for v in gMap.values():
+    for i, ix in enumerate(v[0]): v[1].append(v[1][i] + healths[ix])
+
+largest = max(list(map(len, genes)))
+hMin, hMax = inf, 0
+for _ in range(howStrands):
+    firstLastd = input().split()
+    first = int(firstLastd[0])
+    last = int(firstLastd[1])
+    strand = firstLastd[2]
+    h = getHealth(strand, first, last, largest)
+    hMin, hMax = min(hMin, h), max(hMax, h)
+print(hMin, hMax)
