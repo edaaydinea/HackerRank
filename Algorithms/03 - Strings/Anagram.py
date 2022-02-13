d7bc57f78968391a8b79f79a5ab8d2cc7e269059
Eda AYDIN <edaaydin.ea@outlook.com>
2022-02-14 01:34:17 +0300

    Update Anagram.py

--- Algorithms/03 - Strings/Anagram.py
+++ Algorithms/03 - Strings/Anagram.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #!/bin/python3
 
 import math
@@ -39,3 +40,46 @@ if __name__ == '__main__':
         fptr.write(str(result) + '\n')
 
     fptr.close()
+=======
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'anagram' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+
+def anagram(s):
+    length_s = len(s)
+    if length_s % 2:
+        return -1
+    mid = length_s // 2
+    a = Counter(s[:mid])
+    b = Counter(s[mid:])
+
+    return mid - sum((a & b).values())
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = anagram(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
+>>>>>>> Stashed changes
