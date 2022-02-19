6c9ab00cd553a6eaa909ef13cca39444186d7889
Eda AYDIN <edaaydin.ea@outlook.com>
2022-02-19 15:06:20 +0300

    Add Counting Sort 1.py

---
+++ Algorithms/04 - Sorting/Counting Sort 1.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import os
+
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+
+def countingSort(arr):
+    # Write your code here
+    total = [0] * 100
+    for j in range(0, len(arr)):
+        temp = arr[j]
+        total[temp] += 1
+    return total
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
