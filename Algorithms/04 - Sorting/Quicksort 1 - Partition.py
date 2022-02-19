f409cbc3a902ed1e09ed9a02eab241ec1ebf72a3
Eda AYDIN <edaaydin.ea@outlook.com>
2022-02-19 14:43:58 +0300

    Add Quicksort 1 - Partition.py

---
+++ Algorithms/04 - Sorting/Quicksort 1 - Partition.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import os
+
+
+#
+# Complete the 'quickSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def quickSort(arr):
+    # Write your code here
+    pivot = arr[0]
+    left = []
+    right = []
+    for element in arr:
+        if element < pivot:
+            left.append(element)
+        else:
+            right.append(element)
+    return left + right
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = quickSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
