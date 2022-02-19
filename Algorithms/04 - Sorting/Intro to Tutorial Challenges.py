53708cdbb2160833d75e47bc54a33b65bf8b84c9
Eda AYDIN <edaaydin.ea@outlook.com>
2022-02-19 14:23:55 +0300

    Add Intro to Tutorial Challenges.py via upload

---
+++ Algorithms/04 - Sorting/Intro to Tutorial Challenges.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'introTutorial' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER V
+#  2. INTEGER_ARRAY arr
+#
+
+def introTutorial(V, arr):
+    # Write your code here
+    return arr.index(V)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    V = int(input().strip())
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = introTutorial(V, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
