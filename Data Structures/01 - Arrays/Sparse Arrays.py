367dc1a2f9259b7e7e0ff5782029ca5a932dbc71
Eda AYDIN <edaaydin.ea@outlook.com>
2022-02-20 11:01:30 +0300

    Add Sparse Arrays.py

---
+++ Data Structures/01 - Arrays/Sparse Arrays.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import os
+
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(strings, queries):
+    # Write your code here
+    freq = []
+    for query in queries:
+        matchingStrings = [string for string in strings if string == query]
+        freq.append(len(matchingStrings))
+    return freq
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
