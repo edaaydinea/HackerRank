eebd08a741c5c8756829956225d60ca46836db12
Eda AYDIN <edaaydin.ea@outlook.com>
2022-02-21 21:38:29 +0300

    Add Compare two linked lists.py

---
+++ Data Structures/02 - Linked Lists/Compare two linked lists.py
@@ -0,0 +1,85 @@
+#!/bin/python3
+
+import os
+
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+# Complete the compare_lists function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def compare_lists(llist1, llist2):
+    if not llist1 and not llist2:
+        return 1
+    elif llist1 and llist2:
+        if llist1.data != llist2.data:
+            return 0
+        return compare_lists(llist1.next, llist2.next)
+    else:
+        return 0
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist1_count = int(input())
+
+        llist1 = SinglyLinkedList()
+
+        for _ in range(llist1_count):
+            llist1_item = int(input())
+            llist1.insert_node(llist1_item)
+
+        llist2_count = int(input())
+
+        llist2 = SinglyLinkedList()
+
+        for _ in range(llist2_count):
+            llist2_item = int(input())
+            llist2.insert_node(llist2_item)
+
+        result = compare_lists(llist1.head, llist2.head)
+
+        fptr.write(str(int(result)) + '\n')
+
+    fptr.close()
